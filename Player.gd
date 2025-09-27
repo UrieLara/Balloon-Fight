f1ef98d08f5e9cf3afd251acf38e318ed3983603
Uriel Lara <141281252+UrieLara@users.noreply.github.com>
2025-09-27 12:42:23 -0300

    update scene

--- Player.gd
+++ Player.gd
@@ -4,7 +4,11 @@ var vec_velocity = Vector2.ZERO
 
 const GRAVITY = 400 
 const FLY_IMPULSE = -200
-const FRICTION = 0.05
+const FRICTION = 0.1
+const MAX_WINDOW_X = 1024
+const SIZE_PLAYER_X = 24
+
+
 var balloons = 2
 
 enum player {idle, moving}
@@ -14,12 +18,26 @@ var actual_player_state = player.idle
 
 func _physics_process(delta):
 	set_player_state()
+	teletransport()
 	vec_velocity.y += GRAVITY * delta
 	vec_velocity = move_and_slide(vec_velocity, Vector2.UP)
 	
 	if is_on_floor():
 		vec_velocity.y = 0
+	
+	#	for i in get_slide_count():
+#		var collision = get_slide_collision(i)
+#		if collision:
+#			print("Golpeaste:"+str(collision.collider.name))
+	
+		
 
+func teletransport():
+	if global_position.x < 0-SIZE_PLAYER_X:
+		global_position.x = MAX_WINDOW_X
+	elif global_position.x > MAX_WINDOW_X+SIZE_PLAYER_X:
+		global_position.x = 0
+	
 func _process(delta):
 	play_animations()
 
@@ -48,7 +66,7 @@ func set_player_state():
 				states.right:
 					move(1, true)
 				states.idle:
-					stand()
+					stop()
 
 		player.moving:
 			match event_k:
@@ -67,24 +85,29 @@ func move (direction, flip):
 	$Animations.flip_h = flip	
 	actual_player_state = player.moving
 	
-	
-	
 func fly ():
 	vec_velocity.y = FLY_IMPULSE
 	actual_player_state = player.moving
 	
-	
-func stand():
+func stop():
 	vec_velocity.x = lerp(vec_velocity.x, 0, FRICTION)
+	
 	
 func play_animations():
 	match actual_player_state:
 		player.idle: 
 			if is_on_floor():
-				if balloons == 2:
-					$Animations.play("Idle-2b")	
-				elif balloons == 1:
-					$Animations.play("Idle-1b")
+				if abs(vec_velocity.x) > 5:
+					if balloons == 2:
+						$Animations.play("Brake-2b")	
+					elif balloons == 1:
+						$Animations.play("Brake-1b")
+						
+				if abs(vec_velocity.x) <= 3:
+					if balloons == 2:
+						$Animations.play("Idle-2b")	
+					elif balloons == 1:
+						$Animations.play("Idle-1b")
 			
 			if vec_velocity.y > 0:
 				if balloons == 2:
@@ -98,6 +121,7 @@ func play_animations():
 					$Animations.play("Run-2b")						
 				elif balloons == 1: 
 					$Animations.play("Run-1b")	
+				
 			else:
 				if balloons == 2:
 					$Animations.play("Fly-2b")	
